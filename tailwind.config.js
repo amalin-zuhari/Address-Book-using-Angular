fb30e201850789333538ce56d74d117d5a604760
amalin-zuhari <amalinzuhari.work@gmail.com>
2024-11-13 20:40:35 +0800

    chore: Install and configi=ure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Ensure Tailwind scans your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
